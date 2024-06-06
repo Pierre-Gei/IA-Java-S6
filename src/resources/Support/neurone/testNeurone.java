096c695ba6c411e7cbafa9c450892a2b3289157e
Pierre-Gei <pierregeiguer@gmail.com>
2024-06-06 09:22:27 +0200

    Stats et bruit

--- src/resources/Support/neurone/testNeurone.java
+++ src/resources/Support/neurone/testNeurone.java
@@ -1,47 +1,82 @@
 package resources.Support.neurone;
 
+import java.util.Random;
+
 public class testNeurone
 {
-	public static void main(String[] args)
-	{
-		// Tableau des entrées de la fonction ET (0 = faux, 1 = vrai)
-		final float[][] entrees = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-		
-		// Tableau des sorties de la fonction ET
-		final float[] resultats = {0, 0, 0, 1};
-		
-		// On crée un neurone taillé pour apprendre la fonction ET
-		final iNeurone n = new NeuroneHeaviside(entrees[0].length);
-		//final iNeurone n = new NeuroneSigmoide(entrees[0].length);
-		//final iNeurone n = new NeuroneReLU(entrees[0].length);
-		
-		System.out.println("Apprentissage…");
-		// On lance l'apprentissage de la fonction ET sur ce neurone
-		System.out.println("Nombre de tours : "+n.apprentissage(entrees, resultats));
-		
-		// On affiche les valeurs des synapses et du biais
+ public static void main(String[] args)
+ {
+  final float[][] entrees = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+  final float[] resultats = {0, 0, 0, 1};
 
-		// Conversion dynamique d'une référence iNeurone vers une référence neurone.
-		// Sans cette conversion on ne peut pas accéder à synapses() et biais()
-		// à partir de la référence de type iNeurone
-		// Cette conversion peut échouer si l'objet derrière la référence iNeurone
-		// n'est pas de type neurone, ce qui n'est cependant pas le cas ici
-		final Neurone vueNeurone = (Neurone)n;
-		System.out.print("Synapses : ");
-		for (final float f : vueNeurone.synapses())
-			System.out.print(f+" ");
-		System.out.print("\nBiais : ");
-		System.out.println(vueNeurone.biais());
-		
-		// On affiche chaque cas appris
-		for (int i = 0; i < entrees.length; ++i)
-		{
-			// Pour une entrée donnée
-			final float[] entree = entrees[i];
-			// On met à jour la sortie du neurone
-			n.metAJour(entree);
-			// On affiche cette sortie
-			System.out.println("Entree "+i+" : "+n.sortie());
-		}
-	}
+  // final iNeurone n = new NeuroneSigmoid(entrees[0].length);
+   final iNeurone n = new NeuroneHeaviside(entrees[0].length);
+  // final iNeurone n = new Neurone(entrees[0].length);
+  // final iNeurone n = new NeuroneReLu(entrees[0].length);
+   //final iNeurone n = new NeuroneTanh(entrees[0].length);
+
+  System.out.println("Apprentissage…");
+  System.out.println("Nombre de tours : "+n.apprentissage(entrees, resultats));
+
+  final Neurone vueNeurone = (Neurone)n;
+  System.out.print("Synapses : ");
+  for (final float f : vueNeurone.synapses())
+   System.out.print(f+" ");
+  System.out.print("\nBiais : ");
+  System.out.println(vueNeurone.biais());
+
+  for (int i = 0; i < entrees.length; ++i)
+  {
+   final float[] entree = entrees[i];
+   n.metAJour(entree);
+   System.out.println("Entree "+i+" : "+n.sortie());
+  }
+
+  statistiquesPoids(vueNeurone);
+  ajouterBruit(vueNeurone, 0.1f);
+  System.out.println("Apprentissage…");
+  System.out.println("Nombre de tours : "+n.apprentissage(entrees, resultats));
+  System.out.print("Synapses : ");
+  for (final float f : vueNeurone.synapses())
+   System.out.print(f+" ");
+  System.out.print("\nBiais : ");
+  System.out.println(vueNeurone.biais());
+  for (int i = 0; i < entrees.length; ++i)
+  {
+   final float[] entree = entrees[i];
+   n.metAJour(entree);
+   System.out.println("Entree "+i+" : "+n.sortie());
+  }
+  statistiquesPoids(vueNeurone);
+ }
+
+ public static void statistiquesPoids(Neurone vueNeurone) {
+  float sum = 0;
+  float min = vueNeurone.synapses()[0];
+  float max = vueNeurone.synapses()[0];
+  for (float poids : vueNeurone.synapses()) {
+   sum += poids;
+   min = Math.min(min, poids);
+   max = Math.max(max, poids);
+  }
+  float mean = sum / vueNeurone.synapses().length;
+  float variance = 0;
+  for (float poids : vueNeurone.synapses()) {
+   variance += Math.pow(poids - mean, 2);
+  }
+  variance /= vueNeurone.synapses().length;
+  float stdDev = (float) Math.sqrt(variance);
+
+  System.out.println("Moyenne des poids: " + mean);
+  System.out.println("Poids minimum: " + min);
+  System.out.println("Poids maximum: " + max);
+  System.out.println("Écart type des poids: " + stdDev);
+ }
+
+ public static void ajouterBruit(Neurone vueNeurone, float amplitude) {
+  Random rand = new Random();
+  for (int i = 0; i < vueNeurone.synapses().length; ++i) {
+   vueNeurone.synapses()[i] += amplitude * (rand.nextFloat() * 2 - 1);
+  }
+ }
 }
