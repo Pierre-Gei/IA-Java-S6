d24e36e2ea57e4aca790a63345d001b7b1875fdb
Pierre-Gei <pierregeiguer@gmail.com>
2024-06-08 12:13:53 +0200

    Réseau V1

--- src/resources/Support/Son/DetecteurDeSons.java
+++ src/resources/Support/Son/DetecteurDeSons.java
@@ -18,21 +18,29 @@ public class DetecteurDeSons {
         //(╯°□°)╯︵ ┻━┻
         float [] sinBruite = normalize(FFT("src/resources/Sources_sonores/SinusoideBruit.wav"));
 
-        iNeurone neuroneSinusoide = new NeuroneSigmoid(sinusoide.length);
-        iNeurone neuroneCarre = new NeuroneSigmoid(carre.length);
+        iNeurone[] neurones = new iNeurone[50];
+        for(int i = 0; i < neurones.length/2; i++){
+            neurones[i] = new NeuroneSigmoid(sinusoide.length);
+        }
+        for(int i = neurones.length/2; i < neurones.length; i++){
+            neurones[i] = new NeuroneSigmoid(carre.length);
+        }
 
         //apprentisage
         System.out.println("Apprentissage…");
         float[][] entrees = {sinusoide,sinusoide2,sinusoide3Harmoniques,combinaison,carre};
         float[] resultats = {1,1,1,1,0};
 
-        System.out.println("Nombre de tours : " + neuroneSinusoide.apprentissage(entrees, resultats));
+        for(int i = 0; i < neurones.length/2; i++){
+            System.out.println("Nombre de tours : " + neurones[i].apprentissage(entrees, resultats));
+        }
 
-        entrees = new float[][]{carre,sinusoide2,sinusoide3Harmoniques,bruit,combinaison,sinusoide};
-        resultats = new float[]{1,0,0,0,1,0};
+        entrees = new float[][]{carre,sinusoide2,sinusoide3Harmoniques,combinaison,sinusoide};
+        resultats = new float[]{1,0,0,1,0};
 
-        System.out.println("Nombre de tours : " + neuroneCarre.apprentissage(entrees, resultats));
-
+        for(int i = neurones.length/2; i < neurones.length; i++){
+            System.out.println("Nombre de tours : " + neurones[i].apprentissage(entrees, resultats));
+        }
 
         //menu
         Scanner scanner = new Scanner(System.in);
@@ -50,46 +58,25 @@ public class DetecteurDeSons {
             choix = scanner.nextInt();
             switch(choix){
                 case 1:
-                    neuroneSinusoide.metAJour(sinusoide);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(sinusoide);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, sinusoide);
                     break;
                 case 2:
-                    neuroneSinusoide.metAJour(sinusoide2);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(sinusoide2);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, sinusoide2);
                     break;
                 case 3:
-                    neuroneSinusoide.metAJour(sinusoide3Harmoniques);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(sinusoide3Harmoniques);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, sinusoide3Harmoniques);
                     break;
                 case 4:
-                    neuroneSinusoide.metAJour(bruit);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(bruit);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, bruit);
                     break;
                 case 5:
-                    neuroneSinusoide.metAJour(carre);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(carre);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, carre);
                     break;
                 case 6:
-                    neuroneSinusoide.metAJour(combinaison);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(combinaison);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, combinaison);
                     break;
                 case 7:
-                    neuroneSinusoide.metAJour(sinBruite);
-                    System.out.println("Sortie neurone sin: " + neuroneSinusoide.sortie());
-                    neuroneCarre.metAJour(sinBruite);
-                    System.out.println("Sortie neurone carré: " + neuroneCarre.sortie());
+                    evaluate(neurones, sinBruite);
                     break;
                 case 0:
                     System.out.println("Au revoir");
@@ -99,6 +86,38 @@ public class DetecteurDeSons {
                     break;
             }
         }while(choix != 0);
+    }
+
+    public static void evaluate (iNeurone[] neurones, float[] entrees) {
+        float nombreNeurones = neurones.length;
+        float moyenneSortiesSin = 0;
+        float moyenneSortiesCarre = 0;
+        for (int i = 0; i < neurones.length / 2; i++) {
+            neurones[i].metAJour(entrees);
+            moyenneSortiesSin += neurones[i].sortie();
+        }
+        for (int i = neurones.length / 2; i < neurones.length; i++) {
+            neurones[i].metAJour(entrees);
+            moyenneSortiesCarre += neurones[i].sortie();
+        }
+        moyenneSortiesSin /= nombreNeurones / 2;
+        moyenneSortiesCarre /= nombreNeurones / 2;
+        System.out.println("Moyenne des sorties " + moyenneSortiesSin + " " + moyenneSortiesCarre);
+        if (moyenneSortiesSin > 0.5 && moyenneSortiesCarre > 0.5){
+            System.out.println("C'est une combinaison");
+        }else if(moyenneSortiesSin > 0.5){
+            System.out.println("C'est une sinusoide");
+        }else if (moyenneSortiesCarre > 0.5){
+            System.out.println("C'est un carre");
+        }else {
+            if (moyenneSortiesSin < 0.05 && moyenneSortiesCarre < 0.05){
+                System.out.println("C'est du bruit");
+            } else if (moyenneSortiesSin > moyenneSortiesCarre ) {
+                System.out.println("Il est plus probable que ce soit une sinusoide");
+            } else {
+                System.out.println("Il est plus probable que ce soit un carre");
+            }
+        }
     }
 
     public static float[] FFT(String path) {
